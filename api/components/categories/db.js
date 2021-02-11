039caa6b1895e2b56438bb041006b058502e3406
Kevin Liao <liaok0082@gmail.com>
2021-02-11 17:21:34 -0300

    API working

--- api/components/categories/db.js
+++ api/components/categories/db.js
@@ -1,7 +1,27 @@
 export const categories = [
-    'Breakfast', 
-    'Brunch', 
-    'Lunch', 
-    'Drinks',
-    'Dessers'
+    {
+        key: 1, 
+        category: 'Breakfast',
+        src: 'https://images.pexels.com/photos/3597065/pexels-photo-3597065.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+        key: 2, 
+        category: 'Brunches', 
+        src: 'https://images.pexels.com/photos/1098599/pexels-photo-1098599.jpeg?cs=srgb&dl=pexels-abdallah-maqboul-1098599.jpg&fm=jpg'
+    },
+    {
+        key: 3, 
+        category: 'Meals', 
+        src: 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg'
+    },
+    {
+        key: 4,
+        category: 'Drinks', 
+        src: 'https://images.pexels.com/photos/544961/pexels-photo-544961.jpeg?cs=srgb&dl=pexels-burst-544961.jpg&fm=jpg'
+    },
+    {
+        key: 5,
+        category: 'Desserts', 
+        src: 'https://images.pexels.com/photos/6062009/pexels-photo-6062009.jpeg?cs=srgb&dl=pexels-nicole-michalou-6062009.jpg&fm=jpg'
+    }, 
 ]
--- src/hooks/useCallApi.js
+++ src/hooks/useCallApi.js
@@ -1,14 +1,23 @@
-import { useState } from 'react'; 
+import axios from 'axios'; 
+import { useState, useEffect } from 'react'; 
 
+// Calls API (GET)
 export const useCallApi = apiUrl => {
-    const [loading, load] = useState(true); 
+    const [data, setData] = useState(null); 
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    setTimeout(() => {
-        load(false);
-    }, 1); 
-
-    const error = null; 
-    const data = {}; 
+    useEffect(() => {
+        axios.get(apiUrl)
+            .then(response => {
+                setData(response); 
+                setLoading(false);
+            })
+            .catch(e => {
+                setLoading(false); 
+                setError(e);
+            })
+    }, [])
 
     return [loading, error, data];
 }
